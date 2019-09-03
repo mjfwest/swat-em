da5b1bdb8d8e6ddbfd719c274bf4950ed527b7ab
Martin Baun <martin-baun@arcor.de>
2019-09-03 21:34:53 +0200

    - makefile for windows / pyinstaller

---
+++ make.bat
@@ -0,0 +1,21 @@
+Rem  Do not forget to define the version number in the pack.nsi file
+
+
+del /s /q swat-em
+rmdir /s /q swat-em
+
+del /s /q dist
+rmdir /s /q dist
+del SWAT-EM_portable.zip
+del SWAT-EM_Setup.exe
+
+git clone https://gitlab.com/martinbaun/swat-em.git
+pyinstaller swat-em\swat_em\main.py --name swat-em --noconsole --icon=swat-em\swat_em\ui\icons\app_icon.ico
+xcopy /e /v swat-em\swat_em\ui .\dist\swat-em\ui\
+
+"C:\Program Files (x86)\NSIS\makensisw.exe" pack.nsi
+
+
+cd /D dist
+"C:\Program Files (x86)\7-Zip\7zG.exe" a SWAT-EM_portable.zip swat-em\
+move SWAT-EM_portable.zip ..\
