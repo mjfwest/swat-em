aecd33b238b594dcf22c267968edd8b62c59a7a9
Martin Baun <martin-baun@arcor.de>
2019-09-03 21:38:32 +0200

    - new version - license wasn't correct defined

--- swat_em/__init__.py
+++ swat_em/__init__.py
@@ -2,9 +2,9 @@
 
 import os
 __title__ = 'SWAT-EM - Specific Winding Analyse Tool for electrical machines'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __author__ = 'Martin Baun'
-__license__ = 'GPLv3'
+__license__ = 'MIT License'
 __copyright__ = 'Copyright 2018-2019 Martin Baun'
 
 __dir__ = os.path.dirname(os.path.abspath(__file__))
