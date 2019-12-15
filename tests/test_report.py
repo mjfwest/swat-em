ca2311f482adaa9a7e02c99df440aafbdddef55b
Martin Baun <martin-baun@arcor.de>
2019-12-15 17:39:50 +0100

    Experimental html report

---
+++ tests/test_report.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import importlib
+import swat_em
+importlib.reload(swat_em)
+
+
+from swat_em.datamodel import datamodel
+from swat_em.report import HtmlReport
+
+
+
+
+
+
+
+
+
+
+
+data = datamodel()
+data.genwdg(12, 10, m=3, w=1, layers=2, turns=1)
+data.set_title('12-10 machine')
+data.set_notes('This is an example machine')
+
+rep = HtmlReport(data)
+rep.create()
