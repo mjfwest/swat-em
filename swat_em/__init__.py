a77098ecfb7bc4f7538d504a5359ed91e40636ed
Martin Baun <martin-baun@arcor.de>
2019-12-27 18:38:34 +0100

    code cleanup

--- swat_em/__init__.py
+++ swat_em/__init__.py
@@ -7,8 +7,6 @@ __author__ = 'Martin Baun'
 __license__ = 'MIT License'
 __copyright__ = 'Copyright 2018-2019 Martin Baun'
 
-#  __dir__ = os.path.dirname(os.path.abspath(__file__))
-
 from swat_em.datamodel import datamodel
 
     
