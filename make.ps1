7600373fe7f1c562df555c7b50316374dcb70be4
Martin Baun <mar.baun@googlemail.com>
2020-04-19 13:49:24 +0100

    fix for build system

--- make.ps1
+++ make.ps1
@@ -9,19 +9,27 @@ Remove-Item -Recurse -Force -ErrorAction Ignore SWAT-EM_Setup.exe
 
 
 git pull
+
+# make docs
 .\doc\make.bat latexpdf
 mkdir -Force swat_em\doc
 Copy-Item "doc\build\latex\SWAT-EM.pdf" -Destination "swat_em\doc\SWAT-EM_manual.pdf"
+
+# build to exe
 pyinstaller swat_em\main.py --name swat-em --noconsole  --exclude scipy --exclude matplotlib --icon=swat_em\ui\icons\app_icon.ico
 
-
+# copy necessary data
 Copy-Item "swat_em\ui" -Recurse -Destination "dist\swat-em\ui\"
 Copy-Item "swat_em\doc" -Recurse -Destination "dist\swat-em\doc\"
 
-
+# make installer
 & "C:\Program Files (x86)\NSIS\makensisw.exe" pack.nsi
 
-& "C:\Program Files\7-Zip\7zG.exe" a SWAT-EM_portable.zip dist\swat-em\
+# make portable package (zip)
+cd dist
+Start-Process -Wait "C:\Program Files\7-Zip\7zG.exe" -ArgumentList "a SWAT-EM_portable.zip swat-em\"
+Move-Item SWAT-EM_portable.zip ..\
+cd ..
 
 # clean-up
 Remove-Item -Force -ErrorAction Ignore swat-em.spec
