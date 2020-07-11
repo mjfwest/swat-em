913a34c7f12df543bcc94b3ec0a2f4ba915ef886
martinbaun <mar.baun@googlemail.com>
2020-07-11 10:41:04 +0200

    Update for test cases

--- tests/test_wdggenerator.py
+++ tests/test_wdggenerator.py
@@ -29,7 +29,7 @@ def test1():
     kw7 = -0.0607
     ret = genwdg(Q, 2*p, m, wstep, layers)
     wdglayout = ret['phases']
-    print(wdglayout)
+    #  print(wdglayout)
     assert [U, V, W] == wdglayout
     
 
@@ -53,7 +53,7 @@ def test1():
     assert kw7 == np.round(data.results['kw_el'][idx][2], 4) # phase W
     
     bc, txt = data.get_basic_characteristics()
-    print(bc)
+    #  print(bc)
 
 
 def test2():
@@ -76,7 +76,7 @@ def test2():
     ret = genwdg(Q, 2*p, m, wstep, layers) # 12/10
     wdglayout = ret['phases']
     
-    print(wdglayout)
+    #  print(wdglayout)
     assert [U, V, W] == wdglayout
     
     data = datamodel()
@@ -114,7 +114,7 @@ def test3():
     kw7 = 0.866
     ret = genwdg(Q, 2*p, m, wstep, layers) # 12/10
     wdglayout = ret['phases']
-    print(wdglayout)
+    #  print(wdglayout)
     assert [U, V, W] == wdglayout
 
     data = datamodel()
@@ -151,7 +151,7 @@ def test4():
     kw7 = 0.2588
     ret = genwdg(Q, 2*p, m, wstep, layers) # 12/10
     wdglayout = ret['phases']
-    print(wdglayout)
+    #  print(wdglayout)
     assert [U, V, W] == wdglayout
 
     data = datamodel()
@@ -188,7 +188,7 @@ def test5():
     kw7 = -0.1576
     ret = genwdg(Q, 2*p, m, wstep, layers) # 12/10
     wdglayout = ret['phases']
-    print(wdglayout)
+    #  print(wdglayout)
     assert [U, V, W] == wdglayout
 
     data = datamodel()
@@ -226,7 +226,7 @@ def test6():
     #  wdglayout = genwdg(Q, 2*p, m, wstep, layers) # 12/10
     ret = genwdg(Q, 2*p, m, wstep, layers)
     wdglayout = ret['phases']
-    print(wdglayout)
+    #  print(wdglayout)
     #  pdb.set_trace()
     assert [U, V, W] == wdglayout
 
@@ -265,7 +265,7 @@ def test7():
     ret = genwdg(Q, 2*p, m, wstep, layers) # 12/10
     wdglayout = ret['phases']
     #  wdglayout = overlapping_fractional_slot_dlayer(Q, 2*p, m, wstep)
-    print(wdglayout)
+    #  print(wdglayout)
     #  pdb.set_trace()
     assert [U, V, W] == wdglayout
 
@@ -288,9 +288,109 @@ def test7():
     assert kw7 == np.round(data.results['kw_el'][idx][2], 4) # phase W
 
 
+def test_combinations_table():
+    '''
+    Test a big table of different slot/pole combinations. The winding
+    factors are taken from following paper:
+
+    Libert, F. & Soulard, Juliette. (2004). Investigation on Pole-Slot Combinations for Permanent-Magnet Machines with Concentrated Windings. 
+
+    https://www.researchgate.net/publication/237458473_Investigation_on_Pole-Slot_Combinations_for_Permanent-Magnet_Machines_with_Concentrated_Windings
+    '''
+    print('Test a big table of different slot/pole combinations')
+
+    wf = {}
+    wf[6] = {4:0.866, 8:0.866, 10:0.5, 14:0.5, 16:0.866, 20:0.866 , 22:0.5, 
+             26:0.5 , 28:0.866  , 32:0.866 , 34:0.5 , 38:0.5 , 40:0.866 }
+    wf[9] = {4:0.616, 6:0.866, 8:0.945 , 10:0.945, 12:0.866, 14:0.617, 
+             16:0.328, 20:0.328, 22:0.617, 24:0.866, 26:0.945, 28:0.945, 
+             30:0.866, 32:0.617, 34:0.328, 38:0.328, 40:0.617, 42:0.866}
+    wf[12] = {8:0.866, 10:0.933, 14:0.933, 16:0.866,32:0.866, 34:0.933, 
+              38:0.933, 40:0.866}
+    wf[15] = {8:0.621, 10:0.866, 14:0.951, 16:0.951, 22:0.621, 38:0.621, 
+              40:0.866}
+    wf[18] = {10:0.647, 12:0.866, 14:0.902, 16:0.945, }
+    wf[21] = {14:0.866, 16:0.890, 20:0.953, 22:0.953, 26:0.890, 28:0.866}
+    wf[24] = {14:0.760, 16:0.866, 20:0.933, 22:0.950, 26:0.95, 28:0.933, 
+              32:0.866, 34:0.760}
+    wf[27] = {18:0.866, 20:0.877, 22:0.915, 24:0.945, 26:0.954, 28:0.954, 
+              30:0.945, 32:0.915, 34:0.877, 36:0.866}
+    wf[30] = {20:0.866, 22:0.874, 26:0.936, 28:0.951, 32:0.951, 34:0.936, 
+              38:0.874, 40:0.866}
+    wf[33] = {22:0.866, 26:0.903, 28:0.928, 32:0.954, 34:0.954, 38:0.928, 
+              40:0.903}
+    wf[36] = {24:0.866, 26:0.867, 28:0.902, 30:0.933, 32:0.945, 34:0.953, 
+              38:0.953, 40:0.945, 42:0.933}
+    wf[39] = {26:0.866, 28:0.863, 32:0.918, 34:0.936, 38:0.954, 40:0.954}
+    wf[42] = {28:0.866, 32:0.890, 34:0.913, 38:0.945, 40:0.953}
+
+    wf[45] = {42:0.951, 44:0.955, 46:0.955, 48:0.951, 50:0.945, 52:0.927, 
+              56:0.886, 58:0.859, 60:0.866}
+    wf[48] = {44:0.950, 46:0.954, 50:0.954, 52:0.950, 56:0.933, 58:0.905, 
+              62:0.857, 64:0.866}
+    wf[51] = {44:0.933, 46:0.944, 50:0.955, 52:0.955, 56:0.944, 58:0.933, 
+              62:0.901, 64:0.880, 68:0.866}
+    wf[54] = {42:0.902, 44:0.915, 46:0.930, 48:0.945, 50:0.949, 52:0.954, 
+              56:0.954, 58:0.949, 60:0.945, 62:0.930, 64:0.915, 66:0.902, 
+              68:0.877, 70:0.854, 72:0.866}
+    wf[57] = {44:0.932, 46:0.912, 50:0.937, 52:0.946, 56:0.955, 58:0.955, 
+              62:0.946, 64:0.937, 68:0.912, 70:0.932, 74:0.852, 76:0.866}
+    wf[60] = {44:0.874, 46:0.892, 50:0.933, 52:0.936, 56:0.951, 58:0.954, 
+              62:0.954, 64:0.951, 68:0.936, 70:0.933, 74:0.892, 76:0.874, 
+              80:0.866}
+    wf[63] = {42:0.866, 44:0.850, 46:0.871, 48:0.890, 50:0.905, 52:0.919, 
+              56:0.945, 58:0.948, 60:0.953, 62:0.955, 64:0.955, 66:0.953, 
+              68:0.948, 70:0.945, 74:0.919, 76:0.905, 78:0.890, 80:0.871}
+    wf[66] = {44:0.866, 46:0.849, 50:0.887, 52:0.903, 56:0.928, 58:0.938, 
+              62:0.951, 64:0.954, 68:0.954, 70:0.951, 74:0.938, 76:0.928, 
+              80:0.903}
+    wf[69] = {46:0.866, 50:0.867, 52:0.884, 56:0.914, 58:0.925, 62:0.943, 
+              64:0.949, 68:0.955, 70:0.955, 74:0.949, 76:0.943, 80:0.925}
+    wf[72] = {48:0.866, 50:0.847, 52:0.867, 56:0.902, 58:0.911, 60:0.933, 
+              62:0.933, 64:0.945, 66:0.950, 68:0.953, 70:0.954, 74:0.954, 
+              76:0.953, 78:0.950, 80:0.945}
+    wf[75] = {50:0.866, 52:0.846, 56:0.880, 58:0.895, 62:0.920, 64:0.930, 
+              68:0.945, 70:0.951, 74:0.955, 76:0.955, 80:0.951}
+    wf[78] = {52:0.866, 56:0.863, 58:0.879, 62:0.906, 64:0.918, 68:0.936, 
+              70:0.943, 74:0.952, 76:0.954, 80:0.954}
+    wf[81] = {54:0.866, 56:0.845, 58:0.860, 60:0.877, 62:0.890, 64:0.904, 
+              66:0.915, 68:0.925, 70:0.933, 72:0.945, 74:0.946, 76:0.951, 
+              78:0.954, 80:0.955}
+    wf[84] = {56:0.866, 58:0.845, 62:0.876, 64:0.890, 68:0.913, 70:0.933, 
+              74:0.939, 76:0.945, 80:0.953}
+    wf[87] = {58:0.866, 62:0.859, 64:0.874, 68:0.899, 70:0.910, 74:0.929, 
+              76:0.936, 80:0.947}
+    wf[90] = {60:0.866, 62:0.843, 64:0.859, 66:0.874, 68:0.886, 70:0.902, 
+              74:0.918, 76:0.927, 78:0.936, 80:0.945}
 
 
+    # Some of the windingfactors doesn't match;
+    # these cases are excluded to prevent asserts
+    exclude = [(15,8),  # SWAT-EM finds a winding with a higher windingfactor
+               (15,22), # SWAT-EM finds a winding with a higher windingfactor
+               (15,38), # SWAT-EM finds a winding with a higher windingfactor
+               (18,10), # SWAT-EM finds a winding with a higher windingfactor
+               (57,44), # high windingfactor from the paper can't be reached here
+               (57,70), # high windingfactor from the paper can't be reached here
+               (81,58)] # Wrong last digit in the paper??
 
+    for slot, poles in wf.items():
+        for P, wf in poles.items():
+            if (slot,P) not in exclude:
+                wdg = datamodel()
+                wdg.genwdg(Q = slot, P = P, m = 3, layers = 2, w = 1)
+                wf2 = wdg.get_fundamental_windingfactor()[0]
+                wf2 = np.round(np.abs(wf2), 3)
+                if not np.allclose(wf, wf2, atol = 0.001):
+                    print(slot, P, wf, wf2)
+                assert np.allclose(wf, wf2, atol = 0.001)
+
+
+def test_single_phase():
+    print('Test single phase winding')
+    wdg = datamodel()
+    wdg.genwdg(Q = 4, P = 4, m = 1, layers = 2, w = 1)
+    assert wdg.get_fundamental_windingfactor()[0] == 1.0
 
 
 
@@ -337,7 +437,7 @@ def test8():
 
 
 
-#  if __name__ == '__main__':
+if __name__ == '__main__':
     #  test1()
     #  test2()
     #  test3()
@@ -345,106 +445,13 @@ def test8():
     #  test5()
     #  test6()
     #  test7()
-    #  test8()
+    #  test_combinations_table()
+    test_single_phase()
 
 
-'''
-Test a big table of different slot/pole combinations. The winding
-factors are taken from following paper:
-
-Libert, F. & Soulard, Juliette. (2004). Investigation on Pole-Slot Combinations for Permanent-Magnet Machines with Concentrated Windings. 
-
-https://www.researchgate.net/publication/237458473_Investigation_on_Pole-Slot_Combinations_for_Permanent-Magnet_Machines_with_Concentrated_Windings
-'''
-
-print('Test a big table of different slot/pole combinations')
 
 
-wf = {}
-wf[6] = {4:0.866, 8:0.866, 10:0.5, 14:0.5, 16:0.866, 20:0.866 , 22:0.5, 
-         26:0.5 , 28:0.866  , 32:0.866 , 34:0.5 , 38:0.5 , 40:0.866 }
-wf[9] = {4:0.616, 6:0.866, 8:0.945 , 10:0.945, 12:0.866, 14:0.617, 
-         16:0.328, 20:0.328, 22:0.617, 24:0.866, 26:0.945, 28:0.945, 
-         30:0.866, 32:0.617, 34:0.328, 38:0.328, 40:0.617, 42:0.866}
-wf[12] = {8:0.866, 10:0.933, 14:0.933, 16:0.866,32:0.866, 34:0.933, 
-          38:0.933, 40:0.866}
-wf[15] = {8:0.621, 10:0.866, 14:0.951, 16:0.951, 22:0.621, 38:0.621, 
-          40:0.866}
-wf[18] = {10:0.647, 12:0.866, 14:0.902, 16:0.945, }
-wf[21] = {14:0.866, 16:0.890, 20:0.953, 22:0.953, 26:0.890, 28:0.866}
-wf[24] = {14:0.760, 16:0.866, 20:0.933, 22:0.950, 26:0.95, 28:0.933, 
-          32:0.866, 34:0.760}
-wf[27] = {18:0.866, 20:0.877, 22:0.915, 24:0.945, 26:0.954, 28:0.954, 
-          30:0.945, 32:0.915, 34:0.877, 36:0.866}
-wf[30] = {20:0.866, 22:0.874, 26:0.936, 28:0.951, 32:0.951, 34:0.936, 
-          38:0.874, 40:0.866}
-wf[33] = {22:0.866, 26:0.903, 28:0.928, 32:0.954, 34:0.954, 38:0.928, 
-          40:0.903}
-wf[36] = {24:0.866, 26:0.867, 28:0.902, 30:0.933, 32:0.945, 34:0.953, 
-          38:0.953, 40:0.945, 42:0.933}
-wf[39] = {26:0.866, 28:0.863, 32:0.918, 34:0.936, 38:0.954, 40:0.954}
-wf[42] = {28:0.866, 32:0.890, 34:0.913, 38:0.945, 40:0.953}
 
-wf[45] = {42:0.951, 44:0.955, 46:0.955, 48:0.951, 50:0.945, 52:0.927, 
-          56:0.886, 58:0.859, 60:0.866}
-wf[48] = {44:0.950, 46:0.954, 50:0.954, 52:0.950, 56:0.933, 58:0.905, 
-          62:0.857, 64:0.866}
-wf[51] = {44:0.933, 46:0.944, 50:0.955, 52:0.955, 56:0.944, 58:0.933, 
-          62:0.901, 64:0.880, 68:0.866}
-wf[54] = {42:0.902, 44:0.915, 46:0.930, 48:0.945, 50:0.949, 52:0.954, 
-          56:0.954, 58:0.949, 60:0.945, 62:0.930, 64:0.915, 66:0.902, 
-          68:0.877, 70:0.854, 72:0.866}
-wf[57] = {44:0.932, 46:0.912, 50:0.937, 52:0.946, 56:0.955, 58:0.955, 
-          62:0.946, 64:0.937, 68:0.912, 70:0.932, 74:0.852, 76:0.866}
-wf[60] = {44:0.874, 46:0.892, 50:0.933, 52:0.936, 56:0.951, 58:0.954, 
-          62:0.954, 64:0.951, 68:0.936, 70:0.933, 74:0.892, 76:0.874, 
-          80:0.866}
-wf[63] = {42:0.866, 44:0.850, 46:0.871, 48:0.890, 50:0.905, 52:0.919, 
-          56:0.945, 58:0.948, 60:0.953, 62:0.955, 64:0.955, 66:0.953, 
-          68:0.948, 70:0.945, 74:0.919, 76:0.905, 78:0.890, 80:0.871}
-wf[66] = {44:0.866, 46:0.849, 50:0.887, 52:0.903, 56:0.928, 58:0.938, 
-          62:0.951, 64:0.954, 68:0.954, 70:0.951, 74:0.938, 76:0.928, 
-          80:0.903}
-wf[69] = {46:0.866, 50:0.867, 52:0.884, 56:0.914, 58:0.925, 62:0.943, 
-          64:0.949, 68:0.955, 70:0.955, 74:0.949, 76:0.943, 80:0.925}
-wf[72] = {48:0.866, 50:0.847, 52:0.867, 56:0.902, 58:0.911, 60:0.933, 
-          62:0.933, 64:0.945, 66:0.950, 68:0.953, 70:0.954, 74:0.954, 
-          76:0.953, 78:0.950, 80:0.945}
-wf[75] = {50:0.866, 52:0.846, 56:0.880, 58:0.895, 62:0.920, 64:0.930, 
-          68:0.945, 70:0.951, 74:0.955, 76:0.955, 80:0.951}
-wf[78] = {52:0.866, 56:0.863, 58:0.879, 62:0.906, 64:0.918, 68:0.936, 
-          70:0.943, 74:0.952, 76:0.954, 80:0.954}
-wf[81] = {54:0.866, 56:0.845, 58:0.860, 60:0.877, 62:0.890, 64:0.904, 
-          66:0.915, 68:0.925, 70:0.933, 72:0.945, 74:0.946, 76:0.951, 
-          78:0.954, 80:0.955}
-wf[84] = {56:0.866, 58:0.845, 62:0.876, 64:0.890, 68:0.913, 70:0.933, 
-          74:0.939, 76:0.945, 80:0.953}
-wf[87] = {58:0.866, 62:0.859, 64:0.874, 68:0.899, 70:0.910, 74:0.929, 
-          76:0.936, 80:0.947}
-wf[90] = {60:0.866, 62:0.843, 64:0.859, 66:0.874, 68:0.886, 70:0.902, 
-          74:0.918, 76:0.927, 78:0.936, 80:0.945}
-
-
-# Some of the windingfactors doesn't match;
-# these cases are excluded to prevent asserts
-exclude = [(15,8),  # SWAT-EM finds a winding with a higher windingfactor
-           (15,22), # SWAT-EM finds a winding with a higher windingfactor
-           (15,38), # SWAT-EM finds a winding with a higher windingfactor
-           (18,10), # SWAT-EM finds a winding with a higher windingfactor
-           (57,44), # high windingfactor from the paper can't be reached here
-           (57,70), # high windingfactor from the paper can't be reached here
-           (81,58)] # Wrong last digit in the paper??
-
-for slot, poles in wf.items():
-    for P, wf in poles.items():
-        if (slot,P) not in exclude:
-            wdg = datamodel()
-            wdg.genwdg(Q = slot, P = P, m = 3, layers = 2, w = 1)
-            wf2 = wdg.get_fundamental_windingfactor()[0]
-            wf2 = np.round(np.abs(wf2), 3)
-            if not np.allclose(wf, wf2, atol = 0.001):
-                print(slot, P, wf, wf2)
-            assert np.allclose(wf, wf2, atol = 0.001)
             
 
 
